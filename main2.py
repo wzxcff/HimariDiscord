510eefd2a5bf5993b00e75257d058cc081a95f9e
wzxcff <shevasfloyd@gmail.com>
2024-02-09 23:35:41 +0200

    Latest commit

--- main2.py
+++ main2.py
@@ -22,9 +22,6 @@ help_command = commands.DefaultHelpCommand(
 )
 
 
-user_timer = {}
-
-
 # Setup bot
 Intents: Intents = Intents.default()
 Intents.message_content = True  # NOQA
@@ -33,10 +30,8 @@ Intents.members = True
 bot = commands.Bot(intents=Intents, command_prefix="-", help_command=help_command)
 
 # Set channel for logs:
-log_channel_id = 1205541327148810340
-welcome_channel_id = 1205284046515609710
-monitoring_channel_id = 1205150098435080215
-voice_category_id = 1205150098435080213
+log_channel_id = 1205606154072358942
+welcome_channel_id = 923997595888656404
 temporary_channels = []
 temporary_categories = []
 
@@ -45,11 +40,11 @@ temporary_categories = []
 async def on_voice_state_update(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
     possible_channel_name = f"{member.display_name}'s area"
     if after.channel is not None:
-        if after.channel.name == "temp":
+        if after.channel.name == "Create solo":
             temp_channel = await after.channel.clone(name=possible_channel_name)
             await member.move_to(temp_channel)
             temporary_channels.append(temp_channel.id)
-        if after.channel.name == "teams":
+        if after.channel.name == "Create team":
             temp_category = await after.channel.guild.create_category(name=possible_channel_name)
             temp_channel = await temp_category.create_voice_channel(name="voice")
             await member.move_to(temp_channel)
@@ -72,25 +67,36 @@ async def roll_dice(ctx):
     await ctx.send(str(randint(1, 6)))
 
 
-@bot.command(name='create-channel', help=".create-channel *name*")
-@commands.has_role('Admin')
+@commands.has_any_role('Admin', 'Insider', 'chatmoder')
+@bot.command(name="trust", help="trust *@user*")
+async def trust_this_user(ctx, member: discord.Member, user_name="empty"):
+    await member.add_roles(discord.utils.get(ctx.guild.roles, name="Trusted"))
+    await ctx.send(f"User <@{member.id}> is now trusted!")
+
+
+@commands.has_any_role('Admin', 'Insider', 'chatmoder')
+@bot.command(name="untrust", help="untrust *@user*")
+async def untrust_this_user(ctx, member: discord.Member):
+    await member.remove_roles(discord.utils.get(ctx.guild.roles, name="Trusted"))
+    await ctx.send(f"User <@{member.id}> from now is not trusted!")
+
+
+@bot.command(name='create-channel', help="create-channel *name*")
+@commands.has_any_role('Admin', 'Insider')
 async def create_channel(ctx, channel_name='empty'):
     guild = ctx.message.guild
-    print(ctx.message.content)
     existing_channel = discord.utils.get(guild.channels, name=channel_name)
     if not existing_channel:
-        print(f'Trying to create a new channel: {channel_name}')
         await guild.create_text_channel(channel_name)
         await ctx.send(f"Successfully created a {channel_name} channel")
     if existing_channel:
         await ctx.send("Cannot create a new channel, already exists")
 
 
-@bot.command(name="delete-channel", help=".delete-channel *name*")
-@commands.has_role('Admin')
+@bot.command(name="delete-channel", help="delete-channel *name*")
+@commands.has_any_role('Admin', 'Insider')
 async def deleter_channel(ctx, channels: discord.TextChannel):
     # guild = ctx.guild
-    print(f"channels: {channels}")
     # existing_channel = discord.utils.get(guild.channels, name=channels)
     # if existing_channel:
     await channels.delete()
@@ -104,15 +110,16 @@ async def pong(ctx):
     await ctx.send("pong")
 
 
-@bot.command(name="kick", help="Probably just kicks someone", pass_context=True)
+@bot.command(name="kick", help="kick *@user*", pass_context=True)
 # @commands.has_permissions(kick_members=True)
-@commands.has_role('Admin')
+@commands.has_any_role('Admin', 'Insider')
 async def kick(ctx, *, user: discord.Member):
     reason = "Unfortunately, you are kicked from the server :broken_heart:"
-    await user.kick(reason=None)
     kick_embed = discord.Embed(title=f":boot: Kicked {user.name}!", description=f"Reason: {reason}\nBy: {ctx.author.mention}")
-    await user.send(embed=kick_embed)
     await bot.get_channel(log_channel_id).send(embed=kick_embed)
+    await user.send(embed=kick_embed)
+    await user.kick(reason=reason)
+
 
 
 @bot.command(name="Himari", help="Who is Himari")
@@ -161,7 +168,7 @@ async def on_member_join(member):
     await bot.get_channel(welcome_channel_id).send(f"Greetings, <@{member.id}>!")
     # button = Button(label=f"Kick {member.display_name}", style=discord.ButtonStyle.red)
     # view.add_item(button)
-    embed_message = discord.Embed(title=f"**{member.display_name}** just connected!", description="Click \"Trust\" them if you want give permission to join not temporary channels.")
+    embed_message = discord.Embed(title=f"**{member.display_name}** just connected!", description="\"-trust\" to trust them :white_heart:")
     await member.send("Welcome to <3")
     embed_message.set_author(name="New user", icon_url=member.avatar.url)
     await bot.get_channel(log_channel_id).send(embed=embed_message)  # view=MyView()
